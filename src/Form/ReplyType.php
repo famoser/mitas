7ab5049dc566d67049b29821bfc64d76b22b642c
Florian Moser <git@famoser.ch>
2024-11-06 18:59:46 +0100

    feat: Add new fields & help to reply form

--- src/Form/ReplyType.php
+++ src/Form/ReplyType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\EraEntry;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,10 @@ class ReplyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('timeOffRequests', TextareaType::class, ['required' => false]);
-        $builder->add('absences', TextareaType::class, ['required' => false]);
+        $builder->add('vacationsInEra', CheckboxType::class, ['required' => false, 'help' => 'help.vacations_in_era']);
+        $builder->add('timeOffRequests', TextareaType::class, ['required' => false, 'help' => 'help.time_off_requests']);
+        $builder->add('absences', TextareaType::class, ['required' => false, 'help' => 'help.absences']);
+        $builder->add('comments', TextareaType::class, ['required' => false, 'help' => 'help.comments']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

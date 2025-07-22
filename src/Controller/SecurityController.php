12c73a7e5fc81d1f29b91c2f64dcce1f10a1dff7
Florian Moser <git@famoser.ch>
2025-07-22 15:09:28 +0200

    maint: Fix code style

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Notifier\Recipient\Recipient;
 use Symfony\Component\Routing\Attribute\Route;
